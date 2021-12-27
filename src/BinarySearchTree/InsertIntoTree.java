76aafc7b003472fa93f1cfdcd33ef7b07d982213
Robert Maxwell <robertmaxwell133@gmail.com>
2021-12-27 12:21:52 -0800

    12-27

---
+++ src/BinarySearchTree/InsertIntoTree.java
@@ -0,0 +1,43 @@
+package BinarySearchTree;
+
+public class InsertIntoTree {
+
+     //Definition for a binary tree node.
+     public class TreeNode {
+         int val;
+        TreeNode left;
+         TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+      }
+     public TreeNode insertIntoBST(TreeNode root, int val) {
+          TreeNode node = root;
+          while (node != null) {
+               // insert into the right subtree
+               if (val > node.val) {
+                    // insert right now
+                    if (node.right == null) {
+                         node.right = new TreeNode(val);
+                         return root;
+                    }
+                    else node = node.right;
+               }
+               // insert into the left subtree
+               else {
+                    // insert right now
+                    if (node.left == null) {
+                         node.left = new TreeNode(val);
+                         return root;
+                    }
+                    else node = node.left;
+               }
+          }
+          return new TreeNode(val);
+     }
+
+}
